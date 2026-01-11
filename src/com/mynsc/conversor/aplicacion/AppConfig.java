6c5f04d22bc42a8d6c60a48b141b9a3559518548
mynsc <mcerquin.10@gmail.com>
2026-01-10 19:33:57 -0500

    feat: implement URL assembly with API key integration

--- src/com/mynsc/conversor/aplicacion/AppConfig.java
+++ src/com/mynsc/conversor/aplicacion/AppConfig.java
@@ -3,11 +3,11 @@ package com.mynsc.conversor.aplicacion;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class AppConfig {
-    Dotenv dotenv = Dotenv.configure()
-    .directory("./src")
-            .ignoreIfMalformed()
-            .ignoreIfMissing()
-            .load();
+    private Dotenv dotenv = Dotenv.configure()
+        .directory("./src")
+        .ignoreIfMalformed()
+        .ignoreIfMissing()
+        .load();
     
     public String getKey() {
         return dotenv.get("API_KEY");
--- src/com/mynsc/conversor/aplicacion/DataBaseClient.java
+++ src/com/mynsc/conversor/aplicacion/DataBaseClient.java
@@ -1,5 +1,7 @@
 package com.mynsc.conversor.aplicacion;
 
 public class DataBaseClient {
-    
+    public String createUrl(String apiKey) {
+        return "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
+    }
 }
